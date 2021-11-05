05b7767a264ab1b0e80faf3c8f4fcb487dab3dcd
mikeyrayvon <mikey@interglobal.vision>
2021-11-05 19:03:19 +0100

    add test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-/*
-test('renders learn react link', () => {
+test('renders Color Palettes heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Color Palettes/i);
+  expect(heading).toBeInTheDocument();
+  screen.debug();
 });
-*/
