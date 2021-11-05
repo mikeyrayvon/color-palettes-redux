9197c24c8b46f2f37ac7e5126d9a7dc4aee17bb5
mikeyrayvon <mikey@interglobal.vision>
2021-11-05 01:53:32 +0100

    add height to new color

--- client/src/components/NewColor.tsx
+++ client/src/components/NewColor.tsx
@@ -11,7 +11,7 @@ const NewColor: React.FC<Props> = ({paletteId}) => {
   } = useAppContext()
 
   return (
-    <div className='pb-12'>
+    <div className='pb-12 h-[190px]'>
       <button className='m-4 w-24 h-24 rounded-full bg-gray-200 text-3xl font-bold text-gray-500 flex justify-center items-center shadow-xl' onClick={() => addColor(paletteId)}>+</button>
     </div>
   )
